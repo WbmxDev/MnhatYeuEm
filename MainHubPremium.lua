b2111ffe241f8e27bfd451ebfc25a95c7be3ad1d
WbmxDev <yyyuufufuffudufuf@gmail.com>
2025-09-29 19:57:23 +0700

    Update MainHubPremium.lua

--- MainHubPremium.lua
+++ MainHubPremium.lua
@@ -1,1 +1,15 @@
+--getgenv().team = "Marines" -- Change to "Pirates" if preferred
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
+
+-- Reliable team selection method
+if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)") then
+    repeat
+        wait()
+        local l_Remotes_0 = game.ReplicatedStorage:WaitForChild("Remotes")
+        l_Remotes_0.CommF_:InvokeServer("SetTeam", getgenv().team)
+        task.wait(1)
+    until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)")
+end
+----------------
+task.wait(2)
 loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/ff54e649d342657c"))()
